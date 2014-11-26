f3b5e245d79e631d8734216234ebfb6820a67bd7
borogarr <kgonzalez99@ufl.edu>
2014-11-26 11:39:25 -0500

    BackTracking functional

---
+++ BackTacking/BackTracking.java
@@ -0,0 +1,108 @@
+import java.util.Arrays;
+public class BackTracking {
+
+	private int num = 0;
+	private int[] numArray;
+	
+	
+	public BackTracking(int num) {
+		this.num = num;
+		numArray = new int[2*num];
+		
+		for (int i = 0; i < 2*num; i++) {
+			numArray[i] = 1;
+		}
+	}
+	
+	public boolean checkNumOfDigits() {
+		
+		int[] temp = new int[num*2];
+		System.arraycopy(numArray, 0, temp, 0, numArray.length);
+		
+		Arrays.sort(temp);
+		
+		for (int i = 0; i < 2*num; i = i + 2) {
+			if (temp[i] != temp[i+1]) {
+				return false;
+			}
+			
+			if (i != 2*num -2) {
+				if (temp[i+1] == temp[i+2]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	public void print() {
+		for (int i = 0; i < numArray.length; i++) {
+			System.out.print(numArray[i]);
+		}
+		
+		System.out.println();
+	}
+	
+	public boolean run() {
+	
+		int sum = 0;
+		boolean ret = false;
+		boolean breakOut = false;
+		
+		while (ret != true) {
+			sum = 0;
+			breakOut = false;
+			
+			if (checkNumOfDigits()) {
+				for (int i = 1; i <= num; i++) {
+					for (int j = 0; j < 2*num; j++) {
+						if (numArray[j] == i  && j < 2*num - i - 1) {
+							if (numArray[j] == numArray[j+i+1]) {
+								sum++;
+								break;
+							}
+							else {
+								breakOut = true;
+							}
+						}
+						
+						if (breakOut == true) {
+							break;
+						}
+					}
+				}
+			}
+			
+			if (sum == num) {
+				ret = true;
+				break;
+			}
+			
+			increment(num*2-1);
+			
+		}
+		
+		return ret;
+		
+		
+	}
+	
+	public void increment(int index) {
+		if (numArray[index] == num) {
+			numArray[index] = 1;
+			increment(index - 1);
+		}
+		else {
+			numArray[index]++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		BackTracking back = new BackTracking(3);
+		boolean x = back.run();
+		System.out.println(x);
+		back.print();
+	}
+	
+}
